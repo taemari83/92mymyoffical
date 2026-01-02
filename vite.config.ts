21bba13db6cc47a8481ab31eb375b74cdf896999
taemari83 <taemari83@gmail.com>
2026-01-02 19:25:43 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import angular from '@analogjs/vite-plugin-angular';
+
+export default defineConfig({
+  plugins: [
+    angular({
+      jit: true,
+    }),
+  ],
+  resolve: {
+    mainFields: ['module'],
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
+});
